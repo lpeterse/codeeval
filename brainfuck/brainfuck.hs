a406f4632f53e3fc9a38dc04cce2dd8217955300
Lars Petersen <info@lars-petersen.net>
2016-06-10 06:36:51 +0200

    BF

--- brainfuck/brainfuck.hs
+++ brainfuck/brainfuck.hs
@@ -5,21 +5,25 @@ main :: IO ()
 main  = brainfuck helloWorld
 
 brainfuck :: String -> IO ()
-brainfuck s = i s ([], repeat '\NUL') >> pure ()
+brainfuck s = i s (repeat '\NUL', '\NUL', repeat '\NUL') >> pure ()
 
-i :: Input -> (State, State) -> IO (State, State)
+i :: Input -> (State, Char, State) -> IO (State, Char, State)
 i []     st         = pure st
-i (x:xs) st@(ls,rs) = case x of
-  '>' -> i xs (head rs : ls, tail rs)
-  '<' -> i xs (tail ls, head ls : rs)
-  '+' -> i xs (ls, succ (head rs) : tail rs)
-  '-' -> i xs (ls, pred (head rs) : tail rs)
-  '.' -> putChar (head rs) >> i xs st
-  ',' -> getChar >>= \c-> i xs (ls, c : tail rs)
-  '[' -> loop st (takeWhile (/= ']') xs) >>= i (tail $ dropWhile (/= ']') xs)
+i (x:xs) st@ (l,m,r) = case x of
+  '>' -> i xs (m:l, head r, tail r)
+  '<' -> i xs (tail l, head l, m:r)
+  '+' -> i xs (l, succ m, r)
+  '-' -> i xs (l, pred m, r)
+  '.' -> putChar m >> i xs st
+  ',' -> getChar >>= \c-> i xs (l,c,r)
+  '[' -> i xs lmr
   _   -> i xs st
   where
-    loop tt ys = i xs tt >>= \tt'-> if head (snd tt') == '\NUL' then pure tt' else loop tt' ys
+    loop st'@(p', _, _) ys = do
+      st''@(p'', _, _) <- i xs st'
+      case compare p' p'' of
+        EQ ->
+      >>= \tt'-> if head (snd tt') == '\NUL' then pure tt' else loop tt' ys
 
 helloWorld :: String
 helloWorld  = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.+++."
