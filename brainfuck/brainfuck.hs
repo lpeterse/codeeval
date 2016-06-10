6e7a52fb9a8017d7329e5fc24ee8579b8630c356
Lars Petersen <info@lars-petersen.net>
2016-06-10 06:43:44 +0200

    BF

--- brainfuck/brainfuck.hs
+++ brainfuck/brainfuck.hs
@@ -7,17 +7,18 @@ main  = brainfuck helloWorld
 brainfuck :: String -> IO ()
 brainfuck s = i s (repeat '\NUL', '\NUL', repeat '\NUL') >> pure ()
 
-i :: Input -> (State, Char, State) -> IO (State, Char, State)
-i []     st         = pure st
-i (x:xs) st@ (l,m,r) = case x of
-  '>' -> i xs (m:l, head r, tail r)
-  '<' -> i xs (tail l, head l, m:r)
-  '+' -> i xs (l, succ m, r)
-  '-' -> i xs (l, pred m, r)
-  '.' -> putChar m >> i xs st
-  ',' -> getChar >>= \c-> i xs (l,c,r)
+i :: (State, Char, State, Input) -> IO (State, Char, State, Input)
+i (l,m,r, [])   = pure (l,m,r)
+i (l,m,r, x:xs) = case x of
+  '>' -> i xs (m:l, head r, tail r, xs)
+  '<' -> i xs (tail l, head l, m:r, xs)
+  '+' -> i xs (l, succ m, r, xs)
+  '-' -> i xs (l, pred m, r, xs)
+  '.' -> putChar m >> i (l, m, r, xs)
+  ',' -> getChar >>= \c-> i (l, c, r, xs)
   '[' -> i xs lmr
-  _   -> i xs st
+  ']' -> pure (l, m, r, xs)
+  _   -> i (l, m, r, xs)
   where
     loop st'@(p', _, _) ys = do
       st''@(p'', _, _) <- i xs st'
